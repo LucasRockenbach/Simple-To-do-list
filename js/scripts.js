297e53b43f702b18a90b3a967b328e82b5680fe2
unknown <lucasrockenbachh@gmail.com>
2023-02-08 12:38:30 -0200

    second

--- js/scripts.js
+++ js/scripts.js
@@ -5,8 +5,60 @@ const todoList =document.querySelector('#todo-list');
 const editForm =document.querySelector('#edit-form');
 const editInput =document.querySelector('#edit-input');
 const cancelEditBtn =document.querySelector('#edit-cancel-btn');
+let oldInputValue
 // FUNÇÕES 
-const saveTodo = (text)
+const saveTodo = (text) =>{
+    const todo = document.createElement("div")
+    todo.classList.add("todo")
+
+    const todoTitle = document.createElement("h3")
+    todoTitle.innerText = text
+    todo.appendChild(todoTitle)
+
+    const doneBtn = document.createElement("button")
+    doneBtn.classList.add("finish-todo")
+    doneBtn.innerHTML = ' <i class="fa-solid fa-check"></i>'
+
+    todo.appendChild(doneBtn)
+
+    const editBtn = document.createElement("button")
+    editBtn.classList.add("edit-todo")
+    editBtn.innerHTML = ' <i class="fa-solid fa-pen"></i>'
+
+    todo.appendChild(editBtn)
+ 
+    const deleteBtn = document.createElement("button")
+    deleteBtn.classList.add("remove-todo")
+    deleteBtn.innerHTML = '  <i class="fa-solid fa-xmark"></i>'
+
+    todo.appendChild(deleteBtn)
+
+    todoList.appendChild(todo)
+
+    todoInput.value = "" // deixar o campo de adicionar tarefa vazio após voce enviar 
+    todoInput.focus()
+
+    
+
+    
+}
+const toggleForms = () => {
+    editForm.classList.toggle("hide")
+    todoForm.classList.toggle("hide")
+    todoList.classList.toggle("hide")
+}
+const upDateTodo = (text)=> {
+        const todos = document.querySelectorAll(".todo")
+
+        todos.forEach((todo) =>{
+            let todoTitle = todo.querySelector("h3")
+
+            if(todoTitle.innerText === oldInputValue){
+                todoTitle.innerText = text
+            }
+        })
+
+    }
 
 //EVENTOS
 todoForm.addEventListener("submit", (e) =>{
@@ -17,4 +69,40 @@ todoForm.addEventListener("submit", (e) =>{
     if(inputValeu){
        saveTodo(inputValeu)
     }
+})
+
+document.addEventListener("click", (e) => {
+
+    const targetEl = e.target
+    const parentEl = targetEl.closest("div")
+    let todoTitle;
+
+    if (parentEl && parentEl.querySelector('h3')){
+        todoTitle = parentEl.querySelector("h3").innerText
+    }
+
+    if(targetEl.classList.contains("finish-todo")){
+        parentEl.classList.toggle("done")
+    }
+    if(targetEl.classList.contains("remove-todo")){
+        parentEl.remove()
+    }
+    if(targetEl.classList.contains("edit-todo")){
+        toggleForms()
+        editInput.value = todoTitle
+        oldInputValue = todoTitle
+    }
+    editForm.addEventListener("submit", (e) => {
+        e.preventDefault()
+
+        const editInputValue = editInput.value
+
+        if(editInputValue){
+            //atualizar
+            upDateTodo(editInputValue)
+
+        }
+        toggleForms()
+    })
+        
 })
